bafadb25c0eca8da4c68bb798d694d043387d4be
Sarah Marinoff <sarah@baytek.ca>
2017-07-31 16:23:49 -0400

    Made the dates for discussions more interesting

--- src/Seeders/FakeDataSeeder.php
+++ src/Seeders/FakeDataSeeder.php
@@ -45,32 +45,51 @@ class FakeDataSeeder extends Seeder
     	$content_type = content('content-type/discussion', false);
     	$topics = Topic::all();
     	$members = User::all();
-    	$discussion_ids = collect([]);
+    	$discussions = collect([]);
+
+        $earliest_date = time() - (7*24*60*60); //1 week ago
+        $latest_date = time(); //Now
 
     	foreach(range(1,$total) as $index) {
-    		//Choose a parent at random
-    		//Can choose from discussions if any exist
-    		if ($index > $total/10) {
-    			$parent_id = rand(0,2) ? $discussion_ids->random() : $topics->random()->id;
-    		}
-    		else {
-    			$parent_id = $topics->random()->id;
-    		}
+            $discussion = (factory(Discussion::class)->make());
 
-    		$discussion = (factory(Discussion::class)->make());
+            //Vary the dates, make some appear edited and some appear deleted
+            $discussion->created_at = $this->randomDate($earliest_date, $latest_date);
+            $discussion->updated_at = rand(0,5) ? $discussion->created_at : date('Y-m-d H:i:s', $latest_date);
+
+            if (rand(0,$total/20)) {
+                $discussion->status = Discussion::DELETED;
+            }
+
+            //Save the discussion
     		$discussion->save();
+
+            //Choose a parent at random
+            //Can choose from discussions if any exist
+            if ($index > $total/10 && rand(0,2)) {
+                $parent = $discussions->random();
+            }
+            else {
+                $parent = $topics->random();
+            }
 
     		//Add relationships
     		$discussion->saveRelation('content-type', $content_type);
-    		$discussion->saveRelation('parent-id', $parent_id);
+    		$discussion->saveRelation('parent-id', $parent->id);
 
     		//Add metadata
     		$discussion->saveMetadata('author_id', $members->random()->id);
 
     		//Store the discussion IDs that can be used as parent_ids
-    		$discussion_ids->push($discussion->id);
-    	}
+    		$discussions->push($discussion);
 
-    	//Change some of the updated_at timestamps, so the discussions will appear edited
+            //Update the earliest date so all discussions will be created after it
+            $earliest_date = strtotime($discussion->created_at);
+    	}
+    }
+
+    protected function randomDate($start, $end)
+    {
+        return date('Y-m-d H:i:s', rand($start, $end));
     }
 }
