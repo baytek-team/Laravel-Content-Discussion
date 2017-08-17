7db02fadd79c364d11368dbb78009e923b59b43a
Yvon Viger <yvon@baytek.ca>
2017-08-17 09:45:59 -0400

    Addressing the issue of no results

--- views/discussion/index.blade.php
+++ views/discussion/index.blade.php
@@ -17,59 +17,73 @@
         </div>
     </div>
 @endsection
-
-@section('content')
-<table class="ui selectable very basic table">
-    <thead>
-        <tr>
-            <th class="nine wide">{{ ___('Discussion') }}</th>
-            <th>{{ ___('Date Posted') }}</th>
-            <th class="center aligned collapsing">{{ ___('Actions') }}</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse($discussions as $discussion)
-            <tr class="seven wide" data-discussion-id="{{ $discussion->id }}">
-                <td>
-                    <div class="ui list">
-                        <div class="item">
-                            <div class="content">
-                                <div class="header">
-                                    @if($discussion->metadata('author_id'))
-                                        {{-- <a href="{{ route( 'members.edit', $discussion->metadata('author_id')) }}"> --}}{{ $discussion->metadata('author_id')->name }}{{-- </a> --}}
-                                    @else
-                                        {{ __('Unknown Author') }}
-                                    @endif
-                                    {{ ___('posted in') }}
-                                    @if($discussion->topic)
-                                        <a href="{{ route( 'discussion.index', $discussion->topic->id) }}">{{ $discussion->topic->title }}</a>
-                                    @else
-                                        {{ ___('Parent not found') }}
-                                    @endif
+@if(count($discussions))
+    @section('content')
+    <table class="ui selectable very basic table">
+        <thead>
+            <tr>
+                <th class="nine wide">{{ ___('Discussion') }}</th>
+                <th>{{ ___('Date Posted') }}</th>
+                <th class="center aligned collapsing">{{ ___('Actions') }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($discussions as $discussion)
+                <tr class="seven wide" data-discussion-id="{{ $discussion->id }}">
+                    <td>
+                        <div class="ui list">
+                            <div class="item">
+                                <div class="content">
+                                    <div class="header">
+                                        @if($discussion->metadata('author_id'))
+                                            {{-- <a href="{{ route( 'members.edit', $discussion->metadata('author_id')) }}"> --}}{{ $discussion->metadata('author_id')->name }}{{-- </a> --}}
+                                        @else
+                                            {{ __('Unknown Author') }}
+                                        @endif
+                                        {{ ___('posted in') }}
+                                        @if($discussion->topic)
+                                            <a href="{{ route( 'discussion.index', $discussion->topic->id) }}">{{ $discussion->topic->title }}</a>
+                                        @else
+                                            {{ ___('Parent not found') }}
+                                        @endif
+                                    </div>
+                                    <div class="description">{{ str_limit($discussion->title, 200) }}</div>
                                 </div>
-                                <div class="description">{{ str_limit($discussion->title, 200) }}</div>
                             </div>
                         </div>
-                    </div>
-                </td>
-                <td>{{ $discussion->created_at }}</td>
-                <td class="right aligned collapsing">
-                    <div class="ui compact text menu">
-                        <a href="{{ route('discussion.children', $discussion->id) }}" class="item"><i class="comments icon"></i>{{-- {{ ___('Responses') }} --}}</a>
-                        <a href="{{ route('discussion.edit', $discussion->id) }}" class="item"><i class="pencil icon"></i>{{-- {{ ___('Edit') }} --}}</a>
-                    </div>
-                </td>
-            </tr>
-        @empty
-            <tr>
-                <td colspan="3">
-                    <div class="ui centered">{{ ___('There are no results') }}</div>
-                </td>
-            </tr>
-        @endforelse
-    </tbody>
-</table>
+                    </td>
+                    <td>{{ $discussion->created_at }}</td>
+                    <td class="right aligned collapsing">
+                        <div class="ui compact text menu">
+                            <a href="{{ route('discussion.children', $discussion->id) }}" class="item"><i class="comments icon"></i>{{-- {{ ___('Responses') }} --}}</a>
+                            <a href="{{ route('discussion.edit', $discussion->id) }}" class="item"><i class="pencil icon"></i>{{-- {{ ___('Edit') }} --}}</a>
+                        </div>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="3">
+                        <div class="ui centered">{{ ___('There are no results') }}</div>
+                    </td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 
-{{ $discussions->links('pagination.default') }}
+    {{ $discussions->links('pagination.default') }}
 
-@endsection
+    @endsection
+
+@else
+    @section('outer-content')
+        <div class="ui middle aligned padded grid no-result">
+            <div class="column">
+                <div class="ui center aligned padded grid">
+                    <div class="column">
+                        <h2>{{ ___('We couldn\'t find anything') }}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+@endif
--- views/topic/index.blade.php
+++ views/topic/index.blade.php
@@ -20,49 +20,63 @@
     </div>
 @endsection
 
-@section('content')
-<table class="ui selectable very basic table">
-    <thead>
-        <tr>
-            <th class="seven wide">{{ ___('Title') }}</th>
-            <th>{{ ___('Date Created') }}</th>
-            <th class="center aligned collapsing">{{ ___('Actions') }}</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse($topics as $topic)
-            <tr class="seven wide" data-topic-id="{{ $topic->id }}">
-                <td>{{ $topic->title }}</td>
-
-                <td>{{ $topic->created_at }}</td>
-                <td class="right aligned collapsing">
-                    <div class="ui compact text menu">
-                        <a href="{{ route( 'discussion.index', $topic->id) }}" class="item"><i class="comments icon"></i> {{-- {{ ___('Discussions') }} --}}</a>
-                        <a href="{{ route('discussion.topic.edit', $topic->id) }}" class="item"><i class="pencil icon"></i> {{-- {{ ___('En Button') }} --}}</a>
-                        {{-- @if(is_null($topic->translation()))
-                            <a class="item" href="{{ route('translation.edit', $topic->id) }}">
-                                <i class="add icon"></i>
-                                {{ ___('Fr Button') }}
-                            </a>
-                        @else
-                            <a class="item" href="{{ route('discussion.topic.edit', $topic->translation()) }}">
-                                <i class="pencil icon"></i>
-                                {{ ___('Fr Button') }}
-                            </a>
-                        @endif --}}
-                    </div>
-                </td>
-            </tr>
-        @empty
+@if(count($topics))
+    @section('content')
+    <table class="ui selectable very basic table">
+        <thead>
             <tr>
-                <td colspan="3">
-                    <div class="ui centered">{{ ___('There are no results') }}</div>
-                </td>
+                <th class="seven wide">{{ ___('Title') }}</th>
+                <th>{{ ___('Date Created') }}</th>
+                <th class="center aligned collapsing">{{ ___('Actions') }}</th>
             </tr>
-        @endforelse
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @forelse($topics as $topic)
+                <tr class="seven wide" data-topic-id="{{ $topic->id }}">
+                    <td>{{ $topic->title }}</td>
 
-{{ $topics->links('pagination.default') }}
+                    <td>{{ $topic->created_at }}</td>
+                    <td class="right aligned collapsing">
+                        <div class="ui compact text menu">
+                            <a href="{{ route( 'discussion.index', $topic->id) }}" class="item"><i class="comments icon"></i> {{-- {{ ___('Discussions') }} --}}</a>
+                            <a href="{{ route('discussion.topic.edit', $topic->id) }}" class="item"><i class="pencil icon"></i> {{-- {{ ___('En Button') }} --}}</a>
+                            {{-- @if(is_null($topic->translation()))
+                                <a class="item" href="{{ route('translation.edit', $topic->id) }}">
+                                    <i class="add icon"></i>
+                                    {{ ___('Fr Button') }}
+                                </a>
+                            @else
+                                <a class="item" href="{{ route('discussion.topic.edit', $topic->translation()) }}">
+                                    <i class="pencil icon"></i>
+                                    {{ ___('Fr Button') }}
+                                </a>
+                            @endif --}}
+                        </div>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="3">
+                        <div class="ui centered">{{ ___('There are no results') }}</div>
+                    </td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 
-@endsection
+    {{ $topics->links('pagination.default') }}
+
+    @endsection
+@else
+    @section('outer-content')
+        <div class="ui middle aligned padded grid no-result">
+            <div class="column">
+                <div class="ui center aligned padded grid">
+                    <div class="column">
+                        <h2>{{ ___('We couldn\'t find anything') }}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+@endif
